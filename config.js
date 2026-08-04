0bc0eb084f21b76e581f17fd244b32f934c31005
Isanka-boop <isankamalith5@gmail.com>
2026-08-04 17:09:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    OWNER_NUMBER: "94764642432",
-    OWNER_NUMBERS: ["94764642432", "94789269322"],
+    OWNER_NUMBER: "94763353368",
+    OWNER_NUMBERS: [""],
     OWNER_NAME: "ᴅɪʟsʜᴀ∇",
     PREFIX: ".",
     AUTO_READ_STATUS: false,
