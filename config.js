022a183e045142f9dd9d3eb3992498119b6a0c19
Isanka-boop <isankamalith5@gmail.com>
2026-08-04 17:33:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     OWNER_NUMBER: "94763353368",
     OWNER_NUMBERS: [""],
-    OWNER_NAME: "ᴅɪʟsʜᴀ∇",
+    OWNER_NAME: "ɪꜱᴀɴᴋᴀ∇",
     PREFIX: ".",
     AUTO_READ_STATUS: false,
     AUTO_REACT: true,
@@ -9,11 +9,11 @@ module.exports = {
     AUTO_LIKE_EMOJI: ['❤️','👍','🔥','😍','👏'],
     REACT_EMOJIS: ['❤','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
     // 🆕 Add these for alive command:
-    BOT_NAME: "Ѕнιтѕυ 〽️𝒾𝓃𝒾",
+    BOT_NAME: "ᴢᴇᴛᴀ 〽️𝒾𝓃𝒾",
     version: "v3.0",
-    MODE: "private",
-    FOOTER: "> ㋛ 𝙿σ𝚆є𝚁є𝙳 𝙱у ᴅɪʟsʜα∇",
-    footer: "> ㋛ 𝙿σ𝚆є𝚁є𝙳 𝙱у ᴅɪʟsʜα∇",
+    MODE: "public",
+    FOOTER: "> ㋛ 𝙿σ𝚆є𝚁є𝙳 𝙱у ᴢᴇᴛᴀ∇",
+    footer: "> ㋛ 𝙿σ𝚆є𝚁є𝙳 𝙱у ᴢᴇᴛᴀ∇",
     DESCRIPTION: "A powerful WhatsApp bot.",
-    MENU_IMAGE_URL: "https://i.imgur.com/xyz123.png"
+    MENU_IMAGE_URL: "https://files.catbox.moe/chtymz.jp"
 };
