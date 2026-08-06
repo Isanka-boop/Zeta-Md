a53c56affb94022290089392ee7abdf6d7973395
Isanka-boop <isankamalith5@gmail.com>
2026-08-06 18:24:46 +0530

    Update baduwa.js

--- plugins/baduwa.js
+++ plugins/baduwa.js
@@ -48,3 +48,59 @@ async (robin, mek) => {
         console.log(e)
     }
 });
+
+
+
+// Ponnaya ///////
+
+
+
+
+const { cmd } = require('../command');
+
+const baduwaReplies = [
+    "*කවුද හුත්තෝ පොන්නයා කියලා?* 🤨"
+];
+
+// COMMAND
+cmd({
+    pattern: "ponnaya",
+    alias: ["ponna"],
+    react: "🫵",
+    category: "fun",
+    filename: __filename
+}, 
+async (robin, mek) => {
+    try {
+        const from = mek.key.remoteJid;
+        await robin.sendMessage(from, { react: { text: "🫵", key: mek.key } });
+        
+        const random = baduwaReplies[Math.floor(Math.random() * baduwaReplies.length)];
+        await robin.sendMessage(from, { text: random }, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e)
+    }
+});
+
+// AUTO DETECT
+cmd({
+    on: "body" // සමහර bot වල on: "text" වෙනුවට on: "body"
+}, 
+async (robin, mek) => {
+    try {
+        const from = mek.key.remoteJid;
+        const body = mek.message?.conversation || mek.message?.extendedTextMessage?.text || "";
+        
+        if(!body) return;
+        const text = body.toLowerCase();
+
+        if (text.includes("බඩුව")) {
+            await robin.sendMessage(from, { react: { text: "😏", key: mek.key } });
+            const random = baduwaReplies[Math.floor(Math.random() * baduwaReplies.length)];
+            await robin.sendMessage(from, { text: random }, { quoted: mek });
+        }
+    } catch (e) {
+        console.log(e)
+    }
+});
